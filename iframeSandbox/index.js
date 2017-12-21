94634f81b9fb0a4f7beb57457e16922f2da37848
Gavin Loughridge <gavinlough@gmail.com>
2017-12-21 15:11:10 -0600

    refactored element selector

--- iframeSandbox/index.js
+++ iframeSandbox/index.js
@@ -1,39 +1,36 @@
 let elementOptions = [];
 
 function appendOption(element, index) {
-  console.log('appending', element);
   $('#elementSelector').append($("<option></option>").attr("value", index).text(element));
-  console.log('appended element');
 }
 
 $('iframe').on('load', function() {
   $('iframe').contents().find('body').click(function (event) {
     event.preventDefault();
-    console.log('something got clicked');
-    console.log('i think it was:', event);
-    console.log('or:', event.target);
 
     elementOptions = []
     $('#elementSelector').empty();
     elementOptions.push(event.target);
 
-    console.log('parents are:', $(event.target).parents().toArray());
-    elementOptions = elementOptions.concat($(event.target).parents().toArray());
-
-    console.log('array is:', elementOptions);
-
-    elementOptions.forEach(function(element, index) {
-      console.log('element is:', element);
-      appendOption(element, index);
+    $(event.target).parents().toArray().forEach(function (element) {
+      elementOptions.push(element);
     })
 
-    //appendOption($(event.target));
-    //$(event.target).parents().each(appendOption($(this)));
+    elementOptions.forEach(function(element, index) {
+      appendOption(element, index);
+    })
+  })
+
+  let prev;
+  $('iframe').contents().find('body').mousemove(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    $(prev).css('border', 'none');
+    $(event.target).css('border', '1px solid red');
+    prev = event.target;
   })
 })
 
 $('#applyColor').click(function(event) {
-  console.log('setting:', elementOptions[$('#elementSelector').val()]);
-  console.log('to:', $('#selectedColor').val());
   $(elementOptions[$('#elementSelector').val()]).css({backgroundColor: $('#selectedColor').val()});
 })
