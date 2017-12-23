edcb5bda39d511853217f5a661f38db2344fe62c
Gavin Loughridge <gavinlough@gmail.com>
2017-12-23 14:33:49 -0600

    set up schema to keep track of dom elements in the iframe

--- iframeSandbox/index.js
+++ iframeSandbox/index.js
@@ -1,24 +1,46 @@
-let elementOptions = [];
+let domElements;
+
+function applyIds() {
+  domElements.each(function (index) {
+    $(this).data('dom-id', index);
+    console.log('giving:', $(this));
+    console.log('data:', $(this).data('test'));
+  });
+}
 
 function appendOption(element, index) {
   $('#elementSelector').append($("<option></option>").attr("value", index).text(element));
+}
+
+function populateParentSelector(target) {
+  elementOptions = []
+  $('#elementSelector').empty();
+  elementOptions.push(event.target);
+
+  $(event.target).parents().toArray().forEach(function (element) {
+    elementOptions.push(element);
+  })
+
+  elementOptions.forEach(function(element, index) {
+    appendOption(element, index);
+  })
 }
 
 $('iframe').on('load', function() {
   $('iframe').contents().find('body').click(function (event) {
     event.preventDefault();
 
-    elementOptions = []
-    $('#elementSelector').empty();
-    elementOptions.push(event.target);
+    console.log('target:', $(event.target));
+    console.log('target data:', $(event.target).data('dom-id'));
+    let index = $(event.target).data('dom-id');
+    console.log('target from data is:', domElements.eq(index));
 
-    $(event.target).parents().toArray().forEach(function (element) {
-      elementOptions.push(element);
-    })
+    // setSelected(event.target);
+    //
+    // populateParentSelector(event.target);
 
-    elementOptions.forEach(function(element, index) {
-      appendOption(element, index);
-    })
+
+
   })
 
   let prev;
@@ -29,6 +51,13 @@ $('iframe').on('load', function() {
     $(event.target).css('border', '1px solid red');
     prev = event.target;
   })
+
+  let iframeBody = $('iframe').contents().find('body');
+  domElements = iframeBody.add(iframeBody.find('*'));
+
+  console.log('dom elements are:', domElements);
+
+  applyIds();
 })
 
 $('#applyColor').click(function(event) {
